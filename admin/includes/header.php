c9517c6ba63bc245c45440e6e5b04648807b2921
sixtech <anectussixbert26@gmail.com>
2024-04-03 17:14:25 +0300

    new updated

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,10 +1,10 @@
 <div class="brand clearfix">
 	<center>
-		<a href="#" class="logo" style="font-size:16px;">ST. Bernard Hostel Management System</a></center>
+		<a href="#" class="logo" style="font-size:16px;">JIT-Hostel Management System</a></center>
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		<ul class="ts-profile-nav">
 			<li class="ts-account">
-				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> LOGGED <i class="fa fa-angle-down hidden-side"></i></a>
+				<a href="#"><img src="img/logged.jpg" class="ts-avatar hidden-side" alt=""> LOGGED <i class="fa fa-angle-down hidden-side"></i></a>
 				<ul>
 					
 					<li><a href="logout.php">Logout</a></li>
--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,11 @@
 
 <?php if($_SESSION['id'])
 { ?><div class="brand clearfix">
-		<a href="#" class="logo" style="font-size:14px;">ST.Bernard Hostel Management System</a>
+		<a href="#" class="logo" style="font-size:14px;">JIT-Hostel Management System</a>
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		<ul class="ts-profile-nav">
 			<li class="ts-account">
-				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> LOGGED <i class="fa fa-angle-down hidden-side"></i></a>
+				<a href="#"><img src="img/logged.jpg" class="ts-avatar hidden-side" alt=""> LOGGED <i class="fa fa-angle-down hidden-side"></i></a>
 				<ul>
 					<li><a href="logout.php">Logout</a></li>
 				</ul>
@@ -16,7 +16,7 @@
 <?php
 } else { ?>
 <div class="brand clearfix">
-		<a href="#" class="logo" style="font-size:16px;">ST.Bernard Hostel Management System</a>
+		<a href="#" class="logo" style="font-size:16px;">JIT-Hostel Management System</a>
 		<span class="menu-btn"><i class="fa fa-bars"></i></span>
 		
 	</div>
